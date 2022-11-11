720bd57e0958a302562debd719be5d0ec4d689aa
LM-Yair <yairlazaro@outlook.com>
2022-11-11 13:20:45 -0600

    edit: pequeño reacomodo en los estilos de las tarjetas en la página de Inicio y el Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export const Header = () => {
   const linkClass = "text-neutral-200 p-2 px-4 h-full flex justify-center items-center hover:bg-neutral-200 hover:text-neutral-600";
   return(
-    <header className='h-16 bg-neutral-800 flex justify-end'>
-      <nav className="flex">
+    <header className="pt-4 bg-neutral-800 flex flex-col justify-between items-center gap-4 md:flex-row md:py-0 md:h-20">
+      <h1 className="text-neutral-200 text-xl text-center px-4 md:text-intial">Ingeniería Científica Bionanomolecular SA de CV</h1>
+      <nav className="h-full flex">
 	<Link href='/' className={linkClass}>
 	  Inicio
 	</Link>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,10 +5,10 @@ import { ProductCardProps } from "../interfaces/Product";
 export const ProductCard = ({ id, name, addToCart }: ProductCardProps) => {
   return (
     <article
-      className="flex h-64 w-48 flex-col gap-2 rounded-xl border border-neutral-600 p-2"
+      className="p-4 flex min-h-68 w-52 flex flex-col gap-2 rounded-xl bg-neutral-300"
       id={id}
     >
-      <div className="h-32 w-full rounded-xl bg-neutral-400"></div>
+      <div className="h-36 w-full rounded-xl bg-neutral-400"></div>
       <Link
         href={`/producto/${id}`}
         className="underline-offset-2 hover:underline"
