3b9a7b7327b0793d92ef772c4c64fef3737ea5a5
LM-Yair <yairlazaro@outlook.com>
2022-11-11 08:11:40 -0600

    edit: errores en el diseño de la tarjeta

--- src/components/ProductCardToCart.tsx
+++ src/components/ProductCardToCart.tsx
@@ -10,20 +10,20 @@ type ViewProductProps = {
 export const ProductCardToCart = ({ product, removeProduct }: ViewProductProps) => {
   const {name,slug,inventory,price, id} = product;
   return(
-  <>
-    <div className="w-48 h-56 bg-neutral-400 rounded-xl"></div>
-    <div>
-      <h3 className="text-xl text-center">{name}</h3>
+  <article className="p-4 w-full max-w-[16rem] bg-neutral-300 rounded-xl flex flex-col gap-4 md:flex-row md:max-w-lg lg:max-w-2xl">
+    <div className="w-full h-56 bg-neutral-400 rounded-xl md:max-w-[180px]"></div>
+    <div className="w-full">
+      <h3 className="text-xl">{name}</h3>
       <span className="block text-neutral-400/75">{slug}</span>
       <span className="block text-neutral-600">En existencia: {inventory}</span>
       <span className="block text-neutral-600">MXN {price}</span>
       <div className="flex flex-wrap gap-2">
         <Button 
-	  text="Remover" 
-	  action={() => removeProduct({productId: `${id}`})}
-	/>
+          text="Remover" 
+          action={() => removeProduct({productId: `${id}`})}
+        />
       </div>
     </div>
-  </>
+  </article>
   );
 }
