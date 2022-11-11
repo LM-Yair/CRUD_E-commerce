29487e90c1a93f07ca5d32ca8824210c228760ae
LM-Yair <yairlazaro@outlook.com>
2022-11-11 06:55:07 -0600

    edit: renombrado de variables y exportación del tipado 'ActionExecuted', edit: formateo de código con prettier

--- src/hooks/useProductAction.ts
+++ src/hooks/useProductAction.ts
@@ -1,65 +1,96 @@
-import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
-import {trpc} from "../utils/trpc";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { trpc } from "../utils/trpc";
 
-type InitialValue = { productId: string, action: string, redirectTo?:string}
+export type ActionExecuted = {
+  productId: string;
+  action: string;
+  redirectTo?: string;
+};
 
 type ProductActionParams = {
-  productId:string, 
-  reportAction?:boolean, 
-  redirectTo?: string
-}
+  productId: string;
+  reportAction?: boolean;
+  redirectTo?: string;
+};
 
-export type ProductAction = (params:ProductActionParams) => any;
+export type ProductAction = (params: ProductActionParams) => any;
 
-const initialValue:InitialValue = {productId:'',action:''};
+const actionExecutedInitialValue: ActionExecuted = {
+  productId: "",
+  action: "",
+};
 
 export const useProductAction = () => {
-  const [actionExecuted,setActionExecuted] = useState(initialValue);
+  const [actionExecuted, setActionExecuted] = useState(
+    actionExecutedInitialValue
+  );
   const utils = trpc.useContext();
-  
+
   const router = useRouter();
 
   useEffect(() => {
-    if(actionExecuted.productId !== '' && actionExecuted.redirectTo) router.push(actionExecuted.redirectTo);
-  },[actionExecuted]);
+    if (actionExecuted.productId !== "" && actionExecuted.redirectTo)
+      router.push(actionExecuted.redirectTo);
+  }, [actionExecuted]);
 
-  const addProductToCart: ProductAction = async ({productId, reportAction = true, redirectTo}) => {
-    try{
-      const res = await utils.cart.add.fetch({productId});
-      reportAction && setActionExecuted({
-	productId:productId,
-	action:'added to cart', 
-	redirectTo
-      });
-      return res
-    }catch(err){console.warn('añadir al carrito CATCH: ',{err})}
-  }
+  const addProductToCart: ProductAction = async ({
+    productId,
+    reportAction = true,
+    redirectTo,
+  }) => {
+    try {
+      const res = await utils.cart.add.fetch({ productId });
+      reportAction &&
+        setActionExecuted({
+          productId: productId,
+          action: "added to cart",
+          redirectTo,
+        });
+      return res;
+    } catch (err) {
+      console.warn("añadir al carrito CATCH: ", { err });
+    }
+  };
 
-  const removeProductFromCart: ProductAction = async ({productId, reportAction = true, redirectTo}) => {
-    try{
-      const res = await utils.cart.removeById.fetch({productId})
-      reportAction && setActionExecuted({
-	productId,
-	action:'remove from cart', 
-	redirectTo
-      });
-      return res
-    }catch(err){console.warn('remover del carrito CATCH: ',{err})}
-  }
+  const removeProductFromCart: ProductAction = async ({
+    productId,
+    reportAction = true,
+    redirectTo,
+  }) => {
+    try {
+      const res = await utils.cart.removeById.fetch({ productId });
+      reportAction &&
+        setActionExecuted({
+          productId,
+          action: "remove from cart",
+          redirectTo,
+        });
+      return res;
+    } catch (err) {
+      console.warn("remover del carrito CATCH: ", { err });
+    }
+  };
 
-  const deleteProductFromDB: ProductAction = async ({productId, reportAction = true, redirectTo}) => {
-    try{
-      const res = await utils.product.deleteById.fetch({id:productId});
-      reportAction && setActionExecuted({ productId, action:'remove from DB', redirectTo });
-      return res
-    }catch(err){console.warn('remover del carrito CATCH: ',{err})}
-  }
+  const deleteProductFromDB: ProductAction = async ({
+    productId,
+    reportAction = true,
+    redirectTo,
+  }) => {
+    try {
+      const res = await utils.product.deleteById.fetch({ id: productId });
+      reportAction &&
+        setActionExecuted({ productId, action: "remove from DB", redirectTo });
+      return res;
+    } catch (err) {
+      console.warn("remover del carrito CATCH: ", { err });
+    }
+  };
 
-  return{
+  return {
     actionExecuted,
     addProductToCart,
     deleteProductFromDB,
     removeProductFromCart,
-  }; 
-}
+  };
+};
